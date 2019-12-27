5c89fabf6f6be24a53231ad8beca67f8449e2cda
weizy0219 <weizy@126.com>
2019-12-27 23:28:47 +0800

    init

---
+++ apps/todos/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { UiModule } from '@myorg/ui';
+
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    UiModule,
+    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
